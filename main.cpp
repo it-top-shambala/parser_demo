eeac565423836740926e904c37f6a4b1bc6a70b2
Starinin Andrey <starininandrey@gmail.com>
2023-11-08 09:34:25 +0300

    Add function Parser()

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string Parser(string& source, const char DELIMITER);
+
+int main() {
+
+
+    return 0;
+}
+
+string Parser(string& source, const char DELIMITER) {
+    auto position = source.find(DELIMITER);
+    auto result = source.substr(0, position);
+    source = source.substr(position + 1);
+
+    return result;git 
+}
