2536530928f9f1ba9588f7bf1d47f6a06b45534f
Starinin Andrey <starininandrey@gmail.com>
2023-11-08 09:46:10 +0300

    Add console program

--- main.cpp
+++ main.cpp
@@ -6,7 +6,20 @@ using namespace std;
 string Parser(string& source, const char DELIMITER);
 
 int main() {
+    string input;
+    cout << "Enter text (<name>|<date of birth>|<phone>): ";
+    getline(cin, input);
 
+    const char DELIMITER_TOTAL = '|';
+    const char DELIMITER_DATE = '.';
+
+    auto name = Parser(input, DELIMITER_TOTAL);
+    auto date_of_birth = Parser(input, DELIMITER_TOTAL);
+    auto phone = input;
+
+    cout << "name: " << "\t\t" << name << endl;
+    cout << "date of birth: " << "\t\t" << date_of_birth << endl;
+    cout << "phone: " << "\t\t" << phone << endl;
 
     return 0;
 }
